bfaa1ee542ada5cd7839b862019714eb700a4cc9
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-06-08 14:47:58 +0100

    inner loops added

---
+++ nested_loop.rb
@@ -0,0 +1,18 @@
+sets = 1
+
+while (sets < 5)
+  puts "Sets: #{sets}"
+
+  reps = 1
+  while (reps < 11)
+    print "Rep: #{reps}"
+    reps += 1
+  end
+
+  puts
+
+  sets += 1
+  puts "Have a rest :-)"
+end
+
+puts "Done!"
